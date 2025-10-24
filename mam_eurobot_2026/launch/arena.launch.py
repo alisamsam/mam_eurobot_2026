a0061995de1b82008bc91a9d945d8cff168d6c95
Sidy Thiaw <74788129+disythw@users.noreply.github.com>
2025-10-24 15:16:48 +0200

    Add camera bridge node to launch file
    Added camera bridge node to connect simulation camera output to ROS. So i don't have to run it everytime

--- mam_eurobot_2026/launch/arena.launch.py
+++ mam_eurobot_2026/launch/arena.launch.py
@@ -43,10 +43,23 @@ def generate_launch_description():
             output='screen',
             arguments=['/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist']
         ),
+
+        Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            name='camera_bridge',
+            output='screen',
+            arguments=[
+                '/sim/camera/image@sensor_msgs/msg/Image@ignition.msgs.Image',
+                '--ros-args', '-r', '/sim/camera/image:=/camera/image_raw'
+            ]
+        ),
+        
         Node(
             package='rviz2',
             executable='rviz2',
             name='rviz2',
             output='screen',
         )
+
     ])
