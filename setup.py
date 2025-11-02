931be9bfb17661b0ccb28e6d633b8436e3dee89b
disythw <disythiaw@gmail.com>
2025-11-02 04:07:43 +0000

    add openCV features, spawn all necessary crates, keyboard control,  permanent bridges for camera

--- setup.py
+++ setup.py
@@ -1,25 +1,36 @@
 from setuptools import setup, find_packages
+from glob import glob
+import os
 
-package_name = 'mam_eurobot_2026*'
+package_name = 'mam_eurobot_2026'
 
 setup(
     name=package_name,
     version='0.0.1',
-    packages=find_packages(where='.', include=[package_name]),
+    packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        # index ament
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        # package.xml
         ('share/' + package_name, ['package.xml']),
+        # installe tous les launch
+        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
+        # installe mondes, modèles, etc. (facultatif mais pratique)
+        ('share/' + package_name + '/worlds', glob('worlds/*')),
+        ('share/' + package_name + '/models', glob('models/**/*', recursive=True)),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Freddy Liendo',
     maintainer_email='liendomf@univ-smb.fr',
-    description='This is a template package for the Master Advanced Mechatronics teams preparing for the Eurobot 2026',
+    description='Template package for MAM teams – Eurobot 2026',
     license='Apache License 2.0',
     entry_points={
         'console_scripts': [
-            'py_test = mam_eurobot_2026.py_test:main',
+            'py_test     = mam_eurobot_2026.py_test:main',
+            'key_ctrl    = mam_eurobot_2026.key_ctrl:main',     
+            'half_spin   = mam_eurobot_2026.half_spin:main',
+            'open_cv_cam = mam_eurobot_2026.open_cv_cam:main', 
         ],
     },
 )
